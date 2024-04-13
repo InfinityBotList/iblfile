e6c16ae75093c79fb1f8cdead9af3d3f4b12c03d
cheesycod <cheesycod@outlook.com>
2024-04-13 08:34:30 +0530

    remove useless keycount constant

--- consts.go
+++ consts.go
@@ -1,8 +1,3 @@
 package iblfile
 
 const Protocol = "frostpaw-rev7" // The exact protocol version to use
-
-// The number of keys to encrypt the data with
-//
-// Note that changing keycount does not need a change in protocol version
-const KeyCount = 16
